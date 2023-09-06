5118b300cca76a39f7b61eac1e13255c38b10aa1
LeZellus <matheo.zeller@gmail.com>
2023-09-06 20:29:29 +0200

    :sparkles: Tailwind added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
     "./assets/**/*.js",
     "./templates/**/*.html.twig",
   ],
+  purge: {
+    content : ['./templates/**/*.html.twig'],
+  },
   theme: {
     extend: {},
   },
--- webpack.config.js
+++ webpack.config.js
@@ -53,6 +53,9 @@ Encore
         config.corejs = '3.23';
     })
 
+    // enables PostSCSS support
+    .enablePostCssLoader()
+
     // enables Sass/SCSS support
     .enableSassLoader()
 
